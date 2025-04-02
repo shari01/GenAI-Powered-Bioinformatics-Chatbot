d065ec5195afc4ab959fdd313d2cae6ed923b778
Sheryar Malik <76906989+shari01@users.noreply.github.com>
2025-04-02 12:19:27 -0700

    Update ai_agent.py

--- ai_agent.py
+++ ai_agent.py
@@ -5,14 +5,12 @@ load_dotenv()
 import os
 
 # Set the environment variables explicitly
-os.environ["GROQ_API_KEY"] = "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"
-os.environ["TAVILY_API_KEY"] = "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"
-#os.environ["OPENAI_API_KEY"] = "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"
+os.environ["GROQ_API_KEY"] = "use your api key here"
+os.environ["TAVILY_API_KEY"] = "use your api key here"
 
 # Also assign them to variables if needed later.
 GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
 TAVILY_API_KEY = os.environ.get("TAVILY_API_KEY")
-#OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
 # Step2: Setup LLM & Tools
 from langchain_groq import ChatGroq
@@ -20,7 +18,6 @@ from langchain_openai import ChatOpenAI
 from langchain_community.tools.tavily_search import TavilySearchResults
 
 # Initialize clients with explicit API key passing
-#openai_llm = ChatOpenAI(model="gpt-4o-mini", api_key=OPENAI_API_KEY)
 groq_llm = ChatGroq(model="llama-3.3-70b-versatile", api_key=GROQ_API_KEY)
 
 # Initialize the Tavily search tool; passing tavily_api_key is optional now since it's set in the environment,
@@ -36,8 +33,6 @@ system_prompt = "Act as an AI chatbot who is smart and friendly"
 def get_response_from_ai_agent(llm_id, query, allow_search, system_prompt, provider):
     if provider == "Groq":
         llm = ChatGroq(model=llm_id, api_key=GROQ_API_KEY)
-   # elif provider == "OpenAI":
-       # llm = ChatOpenAI(model=llm_id, api_key=OPENAI_API_KEY)
     else:
         raise ValueError("Unsupported provider")
     
